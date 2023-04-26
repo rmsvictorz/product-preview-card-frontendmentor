d4ecbacb0cd32e7c381063e857f6d9a8589ce2fa
RCVictor <victorz.ramosc@gmail.com>
2023-04-26 01:57:28 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+    <aside className='card'>
+      <picture>
+        <source media='(min-width: 374px)' srcSet='/image-product-desktop.jpg'/>
+        <source media='(max-width: 375px)' srcSet='/image-product-mobile.jpg'/>
+        <img src="/image-product-desktop.jpg" alt="image product" />
+      </picture>
+      <section className='content'>
+        <span>PERFUME</span>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
+        <p>
+          A floral, solar and voluptuous interpretation compose by Oliver Polge, Perfumer-Creator fot the House of CHANEL 
+        </p>
+        <div className='price'>
+          <strong>$149.99</strong>
+          <span>$169.99</span>
+        </div>
+        <button>
+          <img src="icon-cart.svg" alt="" />
+          Add to cart
+        </button>
+      </section>
+    </aside>
+    </>
+  )
+}
+
+export default App
